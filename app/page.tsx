81d65cbf0ce1f28c5611a657f788b34caaf2e3bc
Aniket-Subudh1 <aniketsubudhi00@gmail.com>
2026-06-12 02:02:10 +0530

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,5 @@
-import { redirect } from "next/navigation";
-
 import { HomeLanding } from "@/components/marketing/home-landing";
-import { getDefaultRedirectPath, getServerSession } from "@/lib/server/services/session";
 
-export default async function HomePage() {
-  const session = await getServerSession();
-
-  if (session) {
-    redirect(getDefaultRedirectPath(session.user.roles));
-  }
-
+export default function HomePage() {
   return <HomeLanding />;
 }
